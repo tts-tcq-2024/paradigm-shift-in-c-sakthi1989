4dfb591a8485a04977db91ee696e438bec64b4ed
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-15 10:50:37 +0530

    batteryIsOk.c

--- checker.c
+++ checker.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <assert.h>
- 
-int Check(float Value, float MinValue, float MaxValue, char *ErrMsg)
+/*ErrMsgPrint function is used to print error message
+ErrMsg - Error message
+*/
+void ErrMsgPrint(char *ErrMsg)
+{
+  printf("%s\n", ErrMsg);
+}
+
+ /*
+ CheckRange function is used to validate the range of values 
+ */
+int CheckRangeofValues(float Value, float MinValue, float MaxValue, char *ErrMsg)
 {
     char Ret = 0;
     if(!(Ret = ((Value >= MinValue) && (Value <= MaxValue)) ? 1: 0))
-        printf("%s\n", ErrMsg);
-    
+        ErrMsgPrint(ErrMsg);
     return Ret;
 }
 
+/*batteryIsOk is used to check the range of values of battery temperature, SOC, charge rate 
+temperature - Actual temperature value
+soc - Actual state of charge
+chargeRate - Actual charge rate
+*/
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  return Check(temperature, 0, 45, "Temperature out of range!\n") + Check(soc, 20, 80, "State of Charge out of range!\n") + Check(chargeRate, 0, 0.8, "Charge Rate out of range!\n") ? 1 : 0;
+  return CheckRangeofValues(temperature, 0, 45, "Temperature out of range!\n") + CheckRangeofValues(soc, 20, 80, "State of Charge out of range!\n") + CheckRangeofValues(chargeRate, 0, 0.8, "Charge Rate out of range!\n") ? 1 : 0;
 }
  
 int main() {
