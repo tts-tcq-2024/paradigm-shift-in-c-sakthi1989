b1f633d27ad628c764143b3f6ba8e566895fff7e
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-28 20:32:06 +0530

    LimitChecker.h

--- LimitChecker.h
+++ LimitChecker.h
@@ -1,1 +1,12 @@
-
+typedef struct {
+    float LwrLmt;
+    float UprLmt;
+    float WarLwrLmt;
+    float WarUprLmt;
+    const char* OutOfRngPrintMsgInEng;
+    const char* OutOfRngPrintMsgInGer;
+    const char* WarLwrPrintMsgInEng;
+    const char* WarLwrPrintMsgInGer;
+    const char* WarUprPrintMsgInEng;
+    const char* WarUprPrintMsgInGer;
+} BatteryParamLimits;
