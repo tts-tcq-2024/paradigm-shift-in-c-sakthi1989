071ad2df6a5f6c45b00c00a59c3ae63e5db33dbe
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-28 20:36:28 +0530

    Update and rename LimitChecker.h to Common.h

--- Common.h
+++ Common.h
@@ -10,3 +10,8 @@ typedef struct {
     const char* WarUprPrintMsgInEng;
     const char* WarUprPrintMsgInGer;
 } BatteryParamLimits;
+
+void PrintMsg(const char* PrintMsgInEng, const char* PrintMsgInGer);
+int BatteryLmtCheck(float value, float LwrLmt, float UprLmt, const char* PrintMsgInEng, const char* PrintMsgInGer);
+int ParameterCheck(float value, BatteryParamLimits Limits);
+int batteryIsOk(float temperature, float soc, float chargeRate);
