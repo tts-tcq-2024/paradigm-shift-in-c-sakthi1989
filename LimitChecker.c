797deb4a38e3ef0b7087cb0248d5d1876519eb1f
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-28 21:14:21 +0530

    LimitChecker.c

--- LimitChecker.c
+++ LimitChecker.c
@@ -1,22 +1,11 @@
 #include "Common.h"
 
-int BatteryLmtCheck(float value, float LwrLmt, float UprLmt, const char* PrintMsgInEng, const char* PrintMsgInGer)
-{
-	/*Limit check*/
-    if (value < LwrLmt || value > UprLmt) {
-        PrintMsg(PrintMsgInEng, PrintMsgInGer);
-        return 0;
-    }
-    return 1;
-}
-
 int ParameterCheck(float value, BatteryParamLimits Limits) {
-	/*Warning Limit check*/
-	if (value >= Limits.WarLwrLmt && value < Limits.LwrLmt) {
-        PrintMsg(Limits.WarLwrPrintMsgInEng, Limits.WarLwrPrintMsgInGer);
-    } else if (value > Limits.UprLmt && value <= Limits.WarUprLmt) {
-        PrintMsg(Limits.WarUprPrintMsgInEng, Limits.WarUprPrintMsgInGer);
-	}
 	
-	return BatteryLmtCheck(value, Limits.LwrLmt, Limits.UprLmt, Limits.OutOfRngPrintMsgInEng, Limits.OutOfRngPrintMsgInGer);
+	(value >= Limits.WarLwrLmt && value < Limits.LwrLmt) ?  PrintMsg(Limits.WarLwrPrintMsgInEng, Limits.WarLwrPrintMsgInGer) : \
+	(value > Limits.UprLmt && value <= Limits.WarUprLmt) ? PrintMsg(Limits.WarUprPrintMsgInEng, Limits.WarUprPrintMsgInGer) : 0; 
+	
+	(value < Limits.LwrLmt || value > Limits.UprLmt) ? PrintMsg(Limits.OutOfRngPrintMsgInEng, Limits.OutOfRngPrintMsgInGer) : 0;
+	
+	return (value < Limits.LwrLmt || value > Limits.UprLmt) ? 0 : 1;
 }
