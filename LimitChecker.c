6f0e9c352a13de1a10ec43a8837092576e1ff003
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-28 22:18:31 +0530

    LimitChecker.c

--- LimitChecker.c
+++ LimitChecker.c
@@ -1,10 +1,22 @@
 #include "Common.h"
 
+int CheckAndPrint(float value, float lowerLimit, float upperLimit, const char* lowerMsgEng, const char* lowerMsgGer, const char* upperMsgEng, const char* upperMsgGer) {
+    if (value < lowerLimit) {
+        PrintMsg(lowerMsgEng, lowerMsgGer);
+        return 0;
+    } else if (value > upperLimit) {
+        PrintMsg(upperMsgEng, upperMsgGer);
+        return 0;
+    }
+    return 1;
+}
+
 int ParameterCheck(float value, BatteryParamLimits Limits) {
-	(value >= Limits.WarLwrLmt && value < Limits.LwrLmt) ?  PrintMsg(Limits.WarLwrPrintMsgInEng, Limits.WarLwrPrintMsgInGer) : \
-	(value > Limits.UprLmt && value <= Limits.WarUprLmt) ? PrintMsg(Limits.WarUprPrintMsgInEng, Limits.WarUprPrintMsgInGer) : 0; 
-	
-	(value < Limits.LwrLmt || value > Limits.UprLmt) ? PrintMsg(Limits.OutOfRngPrintMsgInEng, Limits.OutOfRngPrintMsgInGer) : 0;
-	
-	return (value < Limits.LwrLmt || value > Limits.UprLmt) ? 0 : 1;
+    if (!CheckAndPrint(value, Limits.WarLwrLmt, Limits.LwrLmt, Limits.WarLwrPrintMsgInEng, Limits.WarLwrPrintMsgInGer, Limits.OutOfRngPrintMsgInEng, Limits.OutOfRngPrintMsgInGer)) {
+        return 0;
+    }
+    if (!CheckAndPrint(value, Limits.UprLmt, Limits.WarUprLmt, Limits.OutOfRngPrintMsgInEng, Limits.OutOfRngPrintMsgInGer, Limits.WarUprPrintMsgInEng, Limits.WarUprPrintMsgInGer)) {
+        return 0;
+    }
+    return 1;
 }
