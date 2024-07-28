bf56b98fe761dc86d81928fd6ed1ca1bd1dc6c55
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-28 20:30:56 +0530

    PrintMsg.c

--- PrintMsg.c
+++ PrintMsg.c
@@ -1,1 +1,7 @@
-
+void PrintMsg(const char* PrintMsgInEng, const char* PrintMsgInGer) {
+    if (LanguageSel == 0) {
+        printf("%s\n", PrintMsgInEng);
+    } else {
+        printf("%s\n", PrintMsgInGer);
+    }
+}
