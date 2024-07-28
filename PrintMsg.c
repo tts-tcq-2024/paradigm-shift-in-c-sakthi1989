ea454b89ff07dcb4af86cd1c5677fb008bff6958
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-28 20:44:00 +0530

    PrintMsg.c

--- PrintMsg.c
+++ PrintMsg.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "Common.h"
 
+extern int LanguageSel;
+
 void PrintMsg(const char* PrintMsgInEng, const char* PrintMsgInGer) {
     if (LanguageSel == 0) {
         printf("%s\n", PrintMsgInEng);
