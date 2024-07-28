84111406eb627fadba3bc0b98eed9acf27f8d533
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-07-28 21:23:34 +0530

    LimitChecker.c

--- LimitChecker.c
+++ LimitChecker.c
@@ -1,11 +1,14 @@
 #include "Common.h"
 
 int ParameterCheck(float value, BatteryParamLimits Limits) {
-	
+
+#if 0
 	(value >= Limits.WarLwrLmt && value < Limits.LwrLmt) ?  PrintMsg(Limits.WarLwrPrintMsgInEng, Limits.WarLwrPrintMsgInGer) : \
 	(value > Limits.UprLmt && value <= Limits.WarUprLmt) ? PrintMsg(Limits.WarUprPrintMsgInEng, Limits.WarUprPrintMsgInGer) : 0; 
 	
 	(value < Limits.LwrLmt || value > Limits.UprLmt) ? PrintMsg(Limits.OutOfRngPrintMsgInEng, Limits.OutOfRngPrintMsgInGer) : 0;
 	
 	return (value < Limits.LwrLmt || value > Limits.UprLmt) ? 0 : 1;
+#endif
+	return 1; 
 }
