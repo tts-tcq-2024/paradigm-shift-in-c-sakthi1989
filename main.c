2ff724b6ac074069e9122f2b14af93c42b05add6
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-08-04 19:14:33 +0530

    main.c

--- main.c
+++ main.c
@@ -30,16 +30,16 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 int main()
 {
     LanguageSel = 0;
-    //assert(batteryIsOk(25, 70, 0.7));
-    //assert(!batteryIsOk(50, 85, 0));
-    //assert(batteryIsOk(30, 22, 0.5));
-    //assert(batteryIsOk(30, 78, 0.5));
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(30, 22, 0.5));
+    assert(batteryIsOk(30, 78, 0.5));
 
     LanguageSel = 1;
-    //assert(batteryIsOk(25, 70, 0.7));
-    //assert(!batteryIsOk(50, 85, 0));
-    //assert(batteryIsOk(30, 22, 0.5));
-    //assert(batteryIsOk(30, 78, 0.5));
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(30, 22, 0.5));
+    assert(batteryIsOk(30, 78, 0.5));
 
     return 0;
 }
