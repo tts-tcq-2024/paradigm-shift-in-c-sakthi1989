bb47e2ba06961a6b52bad58cf39b9b280318ae5c
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-08-05 07:17:38 +0530

    main.c

--- main.c
+++ main.c
@@ -26,20 +26,19 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 
     return status;
 }
-
-int main()
-{
-    LanguageSel = 0;
+int main() {
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
-    assert(batteryIsOk(30, 22, 0.5));
-    assert(batteryIsOk(30, 78, 0.5));
+    assert(!batteryIsOk(30, 85, 0));
+    assert(!batteryIsOk(25, 70, 0.9));
+    
+    // Warning test cases
+    assert(batteryIsOk(2.15, 70, 0.7)); // Should give a temperature low warning but pass the check
+    assert(batteryIsOk(44.85, 70, 0.7)); // Should give a temperature high warning but pass the check
+    assert(batteryIsOk(25, 23.95, 0.7)); // Should give a SoC low warning but pass the check
+    assert(batteryIsOk(25, 76.05, 0.7)); // Should give a SoC high warning but pass the check
+    assert(batteryIsOk(25, 70, 0.76)); // Should give a charge rate high warning but pass the check
 
-    LanguageSel = 1;
-    assert(batteryIsOk(25, 70, 0.7));
-    assert(!batteryIsOk(50, 85, 0));
-    assert(batteryIsOk(30, 22, 0.5));
-    assert(batteryIsOk(30, 78, 0.5));
-
+    printf("All tests passed!\n");
     return 0;
 }
