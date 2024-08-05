1cfefd995ed9a926860f0e0a98c23109710c097f
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-08-05 07:29:54 +0530

    main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
     Check checks[] = {
         {isTemperatureInRange, temperature, 2.25, "Temperature out of range!\n", "Warning: Approaching low temperature limit!\n", "Warning: Approaching high temperature limit!\n"},
         {isSocInRange, soc, 4.0, "State of Charge out of range!\n", "Warning: Approaching low SoC limit!\n", "Warning: Approaching high SoC limit!\n"},
-        {isChargeRateInRange, chargeRate, 0.04, "Charge Rate out of range!\n", NULL, "Warning: Approaching high charge rate limit!\n"}
+        {isChargeRateInRange, chargeRate, 0.04, "Charge Rate out of range!\n", "Warning: Approaching low charge rate limit!\n, "Warning: Approaching high charge rate limit!\n"}
     };
 
     float minLimits[] = {0, 20, 0};
