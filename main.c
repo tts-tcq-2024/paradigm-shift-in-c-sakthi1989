1add26a4ac0f1c3c3d5a5f106e28d036d32ecb63
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-08-05 07:19:37 +0530

    main.c

--- main.c
+++ main.c
@@ -4,28 +4,24 @@
 int LanguageSel = 0;
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    BatteryParamLimits TemperatureLimits = {0, 45, 40, 45, \
-	"Temperature out of range!", "Temperatur außerhalb des Bereichs!", \
-	"Warning: Temperature is decreasing", "Warnung: Die Temperatur sinkt", \
-	"Warning: Temperature is increasing", "Warnung: Die Temperatur steigt"};
-    
-	BatteryParamLimits SocLimits = {20, 80, 24, 76, \
-	"State of Charge out of range!", "Ladezustand außerhalb des Bereichs!", \
-	"Warning: Approaching discharge", "Warnung: Herannahende Entladung", \
-	"Warning: Approaching charge peak", "Warnung: Annäherung an die Ladespitze"};
-    
-	BatteryParamLimits ChargeRateLimits = {0, 0.8, 0.76, 0.8, \
-	"Charge Rate out of range!", "Laderate außerhalb des Bereichs!", \
-	"Warning: Charge Rate is decreasing", "Warnung: Die Laderate sinkt", \
-	"Warning: Charge Rate is increasing", "Warnung: Die Laderate steigt"};
+    Check checks[] = {
+        {isTemperatureInRange, temperature, 2.25, "Temperature out of range!\n", "Warning: Approaching low temperature limit!\n", "Warning: Approaching high temperature limit!\n"},
+        {isSocInRange, soc, 4.0, "State of Charge out of range!\n", "Warning: Approaching low SoC limit!\n", "Warning: Approaching high SoC limit!\n"},
+        {isChargeRateInRange, chargeRate, 0.04, "Charge Rate out of range!\n", NULL, "Warning: Approaching high charge rate limit!\n"}
+    };
 
-   int status = 1;
-    status &= ParameterCheck(temperature, TemperatureLimits);
-    status &= ParameterCheck(soc, SocLimits);
-    status &= ParameterCheck(chargeRate, ChargeRateLimits);
+    float minLimits[] = {0, 20, 0};
+    float maxLimits[] = {45, 80, 0.8};
 
-    return status;
+    for (int i = 0; i < sizeof(checks) / sizeof(checks[0]); ++i) {
+        if (!performCheck(&checks[i], minLimits[i], maxLimits[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
 }
+
 int main() {
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
@@ -33,12 +29,11 @@ int main() {
     assert(!batteryIsOk(25, 70, 0.9));
     
     // Warning test cases
-    assert(batteryIsOk(2.15, 70, 0.7)); // Should give a temperature low warning but pass the check
-    assert(batteryIsOk(44.85, 70, 0.7)); // Should give a temperature high warning but pass the check
-    assert(batteryIsOk(25, 23.95, 0.7)); // Should give a SoC low warning but pass the check
-    assert(batteryIsOk(25, 76.05, 0.7)); // Should give a SoC high warning but pass the check
-    assert(batteryIsOk(25, 70, 0.76)); // Should give a charge rate high warning but pass the check
-
-    printf("All tests passed!\n");
+    assert(batteryIsOk(2.15, 70, 0.7)); 
+    assert(batteryIsOk(44.85, 70, 0.7)); 
+    assert(batteryIsOk(25, 23.95, 0.7)); 
+    assert(batteryIsOk(25, 76.05, 0.7));
+    assert(batteryIsOk(25, 70, 0.76)); 
+	
     return 0;
 }
