f9ce2c986775cd9e3ed5632ec684a09b306bb924
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-08-11 21:08:07 +0530

    Common.h

--- Common.h
+++ Common.h
@@ -3,7 +3,5 @@ typedef enum {
     GERMAN
 } Language;
 
-Language language = ENGLISH;
-
 void PrintMessage(const char* msg_en, const char* msg_de);
 int OutOfRange(float val, float min, float max, const char* msg_en, const char* msg_de);
