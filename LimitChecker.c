a2792af32168486891ebcb4f883a0a863197242f
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-08-11 21:06:32 +0530

    LimitChecker.c

--- LimitChecker.c
+++ LimitChecker.c
@@ -1,1 +1,11 @@
+#include "Common.h"
 
+int OutOfRange(float val, float min, float max, const char* msg_en, const char* msg_de) 
+{
+    if (val < min || val > max)
+    {
+        PrintMessage(msg_en, msg_de);
+        return 0;
+    }
+    return 1;
+}
