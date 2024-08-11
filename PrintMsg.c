25858eae71daa91d16ff5240a8eeeb0e74c13d8b
sakthi1989 <51967798+sakthi1989@users.noreply.github.com>
2024-08-11 21:06:00 +0530

    PrintMsg.c

--- PrintMsg.c
+++ PrintMsg.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+void PrintMessage(const char* msg_en, const char* msg_de)
+{
+    switch (language) 
+    {
+        case 0:
+            printf("EN: %s\n", msg_en);
+            break;
+        case 1:
+            printf("DE: %s\n", msg_de);
+            break;
+        default:
+            printf("Language not supported\n");
+            break;
+    }
+}
